a0a372f7220a26e132362d6dc4796fd33cb8ca37
Ben Keener <keener4christ@gmail.com>
2020-03-09 21:46:12 +0000

    Completed task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Traverses a binary tree in postorder
+ * @tree: The current node
+ * @func: Pointer to the function to call on every value in the tree
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
