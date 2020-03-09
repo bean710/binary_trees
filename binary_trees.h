b5ed9e1ed575468568fba96299169ec3b69e237c
Ben Keener <keener4christ@gmail.com>
2020-03-09 21:11:57 +0000

    Removed extra prototype

--- binary_trees.h
+++ binary_trees.h
@@ -29,6 +29,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-
 #endif
