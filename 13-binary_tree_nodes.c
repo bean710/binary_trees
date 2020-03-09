52f91592b2dd15d57cc1e2b71302c5950fcb34e2
Nehal Shastri <nhlshstr@gmail.com>
2020-03-09 15:28:23 -0700

    works, no betty

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/*
+ * binary_tree_nodes - counts nodes w/ atleast 1 child
+ * @tree: Points to root node of tree
+ * Return: 0 on failure, count on success.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
