8b1368a53660f34495eae007cd23749c4567f91e
Ben Keener <keener4christ@gmail.com>
2020-03-09 21:54:20 +0000

    Completed task 9

--- binary_trees.h
+++ binary_trees.h
@@ -32,5 +32,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif
