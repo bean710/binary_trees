6b575a613439ffd497d991d736b4eefee6288368
Nehal Shastri <nhlshstr@gmail.com>
2020-03-11 15:53:56 -0700

    initial commit for 24

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+/**
+ * BSTHelper - Checks if the tree is a BST
+ * @tree: Parent node
+ * @prev: Previous node
+ * Return: Boolean value of the BST status of tree
+ */
+int BSTHelper(const binary_tree_t *tree, const binary_tree_t *prev)
+{
+	if (tree)
+	{
+		if (!BSTHelper(tree->left, prev))
+			return (0);
+		if (prev != NULL && tree->n <= prev->n)
+			return (0);
+		prev = tree;
+
+		return (BSTHelper(tree->right, prev));
+	}
+
+	return (1);
+}
+/**
+ * binary_tree_is_bst - Checks if the tree is a BST
+ * @tree: Parent node
+ * Return: Boolean value of the BST status of tree
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	const binary_tree_t *prev = NULL;
+
+	return (BSTHelper(tree, prev));
+}
