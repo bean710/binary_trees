ad3e886bab96a251ffebdaa0f2ce03bfda4b181c
Nehal Shastri <nhlshstr@gmail.com>
2020-03-11 16:05:44 -0700

    removed check hunting NULL statements

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,11 +8,6 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {	
-	if (!tree)
-		return (NULL);
-	if (tree->right)
-		return (tree);
-
 	binary_tree_t *node = tree->right;
 
 	tree->right = node->left;
