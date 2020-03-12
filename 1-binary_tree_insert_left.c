250d7fe95360f90910a07b6b6168eb74be039179
Ben Keener <keener4christ@gmail.com>
2020-03-12 16:23:42 +0000

    Fixed memory leaks

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,14 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
+	
+	if (!parent)
+		return (NULL);
 
-	if (!parent || !node)
+	node = malloc(sizeof(binary_tree_t));
+
+	if (!node)
 		return (NULL);
 
 	node->n = value;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,11 +9,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *right_node;
 
+	if (!parent)
+		return (NULL);
+
 	right_node = binary_tree_node(parent, value);
 
 	if (!right_node)
-		return (NULL);
-	if (!parent)
 		return (NULL);
 
 	if (parent->right)
