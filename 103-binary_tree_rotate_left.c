2080a6be1b089fbf69183d130045616fd7db8a69
Nehal Shastri <nhlshstr@gmail.com>
2020-03-11 18:22:09 -0700

    fixed bad dereference

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,17 +8,18 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {	
-	binary_tree_t *node = tree->right;
+	binary_tree_t *node = NULL;
 	
 	if (!tree || !tree->right)
-		return (NULL);
+		return (tree);
+
+	node = tree->right;
 
 	tree->right = node->left;
 	if (node->left)
 		node->left->parent = tree;
 	node->left = tree;
 	tree->parent = node;
-	node->parent = NULL;
 
 	return (node);
 }
