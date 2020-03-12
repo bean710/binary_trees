eaa405a363b51ca2a1973a50fb23685eb22d6ca8
Nehal Shastri <nhlshstr@gmail.com>
2020-03-11 17:21:47 -0700

    edge case modified

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -10,8 +10,8 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {	
 	binary_tree_t *node = tree->right;
 	
-	if (!tree)
-		return (tree);
+	if (!tree || !tree->right)
+		return (NULL);
 
 	tree->right = node->left;
 	if (node->left)
