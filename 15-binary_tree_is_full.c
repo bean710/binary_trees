2947e3900e173486a3f6a1bebe2f07055787dca4
Ben Keener <keener4christ@gmail.com>
2020-03-12 16:37:36 +0000

    Fixed NULL root case

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,11 +9,11 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (1);
-
-	if ((tree->left || tree->right) && !(tree->left && tree->right))
 		return (0);
 
-	return (binary_tree_is_full(tree->left) &
-			binary_tree_is_full(tree->right));
+	if ((tree->left || tree->right) && !(tree->left && tree->right))
+		return (-1);
+
+	return (binary_tree_is_full(tree->left) + 1 &
+			binary_tree_is_full(tree->right) + 1);
 }
