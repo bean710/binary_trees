f34242d4dcd958a449a91e791dd63506fbb39251
Nehal Shastri <nhlshstr@gmail.com>
2020-03-11 18:23:53 -0700

    Completed task 22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,9 +7,9 @@
  * Return: Pointer to the new root
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
-{	
+{
 	binary_tree_t *node = NULL;
-	
+
 	if (!tree || !tree->right)
 		return (tree);
 
