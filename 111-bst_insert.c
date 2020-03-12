65abd6ce6fc669fee67092607d6b176e98359b6a
Nehal Shastri <nhlshstr@gmail.com>
2020-03-11 21:12:07 -0700

    initial commit

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,54 +1,28 @@
 #include "binary_trees.h"
-
-bst_t *bst_recurse_insert(bst_t *tree, int value)
-{
-	if (!tree->left && value < tree->n)
-	{
-		tree->left = malloc(sizeof(bst_t));
-		if (!tree->left)
-			return (NULL);
-		tree->left->n = value;
-		tree->left->left = NULL;
-		tree->left->right = NULL;
-		tree->left->parent = tree;
-		return (tree->left);
-	}
-	else if (!tree->right && value > tree->n)
-	{
-		tree->right = malloc(sizeof(bst_t));
-		if (!tree->right)
-			return (NULL);
-		tree->right->n = value;
-		tree->right->right = NULL;
-		tree->right->right = NULL;
-		tree->right->parent = tree;
-		return (tree->right);
-	}
-
-	if (value < tree->n)
-		return (bst_recurse_insert(tree->left, value));
-	else if (value > tree->n)
-		return (bst_recurse_insert(tree->right, value));
-
-	return (NULL);
-}
-
+/**
+ * bst_insert - Inserts node into tree
+ * @tree: Double pointer to root node
+ * @value: Value to be added to the node
+ * Return: Pointer to new node
+ */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	if (!tree)
-		return (NULL);
+	bst_t *newNode = binary_tree_node(*tree, value);
+	bst_t *x = *tree;
 
-	if (!*tree)
+	while (x != NULL)
 	{
-		*tree = malloc(sizeof(bst_t));
-		(*tree)->left = NULL;
-		(*tree)->right = NULL;
-		(*tree)->parent = NULL;
-		(*tree)->n = value;
-
-		return (*tree);
+		if (value < x->n)
+			x = x->left;
+		else
+			x = x->right;
 	}
 
-	return (bst_recurse_insert(*tree, value));
+	if (x == NULL)
+		x = newNode;
+	else if (value < x->n)
+		x->left = newNode;
+	else
+		x->right = newNode;
+	return (x);
 }
-
