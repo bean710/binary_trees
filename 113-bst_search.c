1d5cd06845252a87ee59b4b433d712e70a7d965c
Ben Keener <keener4christ@gmail.com>
2020-03-13 03:50:19 +0000

    Completed task 27

---
+++ 113-bst_search.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - Seaches a binary search tree for a value
+ * @tree: Pointer to the node currently searching on
+ * @value: The value to seach for
+ *
+ * Return: Pointer to the node with the value, NULL if not found
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+
+	if (value > tree)
+		return (bst_search(tree->right, value));
+
+	if (value < tree)
+		return (bst_search(tree->left, value));
+
+	return (tree);
+}
